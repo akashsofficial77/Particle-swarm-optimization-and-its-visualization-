3977023fd34ce2e7c521d24979326ab66a8d9a0b
Srivastava_Akash_001388877 <srivastava.aka@husky.neu.edu>
2019-08-13 19:02:13 -0400

    udjusting weights

--- src/pso/Constants.java
+++ src/pso/Constants.java
@@ -10,9 +10,9 @@ public class Constants {
 
 	
 	public static final double INERTIA_FACTOR = .729;
-	public static final double COGNITIVE_WEIGHT = .1;
-	public static final double SOCIAL_WEIGHT = 1.49445;
-	public static final double GLOBAL_WEIGHT = 0.3645;
+	public static final double COGNITIVE_WEIGHT = 0.1;
+	public static final double SOCIAL_WEIGHT = .01;
+	public static final double GLOBAL_WEIGHT = 0.8;
 	public static final int PARTICLE_UPPER_BOUND = 1000;
 
 	public Constants() {
